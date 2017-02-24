8dab900acae534f05310c5985e46f570eca9709b
James Talarico <james.v.talarico@gmail.com>
2017-02-23 19:17:07 -0500

    Nothing is working

--- src/pages/manage/manage.ts
+++ src/pages/manage/manage.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import {InAppBrowser} from 'ionic-native';
+import {BankTransfer} from '../../providers/bank-transfer';
 
+//import { Config } from '../env';  // Fast workaround for global variables. Use apiUrl
+//let config = new Config();
 /*
   Generated class for the Manage page.
 
@@ -9,16 +13,37 @@ import { NavController, NavParams } from 'ionic-angular';
 */
 @Component({
   selector: 'page-manage',
-  templateUrl: 'manage.html'
+  templateUrl: 'manage.html',
+  providers: [BankTransfer]
 })
 export class ManagePage {
+  data: any;
+  message: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ManagePage');
+  constructor(public navCtrl: NavController, public navParams: NavParams, public bankTransferService: BankTransfer) {
+    this.test();
+  }
+    ionViewDidLoad() {
+      console.log('ionViewDidLoad ManagePage');
   }
 
   currencies: string = "CAD";
   action: string="add_money";
+
+  public withdraw() {
+
+    //let browser = new InAppBrowser(config.apiUrl, '_system');
+  }
+
+
+  public test() {
+    this.bankTransferService.post()
+      .then(data => {
+        this.data = data;
+        this.message = this.data.message;
+      });
+  }
+
 }
+
+
