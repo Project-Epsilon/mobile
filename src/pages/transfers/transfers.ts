f784d97248dd84be06efb73e565fd6e00f7ad754
Peter0892 <peter_04man@hotmail.com>
2017-02-25 14:59:05 -0500

    Working on modal to respect original layout

--- src/pages/transfers/transfers.ts
+++ src/pages/transfers/transfers.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, } from 'ionic-angular';
+import { NavController, NavParams, ModalController} from 'ionic-angular';
 import { TransfersModalPage} from "../modals/transfers-modal/transfers-modal";
 
 /*
@@ -14,14 +14,15 @@ import { TransfersModalPage} from "../modals/transfers-modal/transfers-modal";
 })
 export class TransfersPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TransfersPage');
   }
 
   showTransferModal() {
-    this.navCtrl.push(TransfersModalPage);
+    let modal = this.modalCtrl.create(TransfersModalPage);
+    modal.present();
   }
 
   currencies: string = "CAD";
