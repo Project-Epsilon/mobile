5092b73b116b6367fecfc636ef763ec13ba23631
Laurendy Lam <lamlaurendy@gmail.com>
2017-02-25 13:09:33 -0500

    authentication

--- src/providers/auth.service.ts
+++ src/providers/auth.service.ts
@@ -1,20 +1,14 @@
 import { Storage } from '@ionic/storage';
-import { AuthHttp, JwtHelper, tokenNotExpired } from 'angular2-jwt';
+import { AuthHttp, tokenNotExpired } from 'angular2-jwt';
 import { Injectable, NgZone } from '@angular/core';
 
 import { environment } from '../environments/environment';
-import { App } from "ionic-angular";
-import { LoginPage } from "../pages/login/login";
-import { TabsPage } from "../pages/tabs/tabs";
 
 declare let Auth0: any;
 declare let Auth0Lock: any;
 
 @Injectable()
 export class AuthService {
-
-  // Angular JWT Helper
-  private jwtHelper: JwtHelper = new JwtHelper();
 
   // Auth0
   public auth0 = new Auth0({
@@ -28,10 +22,9 @@ export class AuthService {
       environment.auth0_domain, {
         auth: {
           redirect: false,
-        },
-        params: {
-          scope: 'openid offline_access',
-          device: 'Mobile Phone'
+          params: {
+            scope: 'openid profile'
+          }
         }
       }
   );
@@ -39,14 +32,10 @@ export class AuthService {
   public user: Object;
   public idToken: string;
 
-  constructor(
-      private authHttp: AuthHttp,
-      public zone: NgZone,
-      public storage: Storage
-  ) {
+  constructor(private authHttp: AuthHttp, public zone: NgZone, public storage: Storage) {
 
     // Check if there is a profile saved in local storage
-    this.storage.get('profile').then(profile => {
+    this.storage.get('user').then(profile => {
       this.user = JSON.parse(profile);
     }).catch(error => {
       console.log(error);
@@ -56,31 +45,26 @@ export class AuthService {
       this.idToken = token;
     });
 
+    //Authenticate user and change jwt key with server key.
     this.lock.on('authenticated', result => {
+      this.storage.set('id_token', result.idToken).then(() => {
+        this.authHttp.post(environment.server_url + '/api/login/auth0', {})
+            .subscribe(res => {
+              let data = res.json().data;
 
-      this.storage.set('id_token', result.idToken);
-      this.idToken = result.idToken;
+              console.log(data);
 
-      console.log(result);
+              //Set token
+              this.idToken = data.token;
+              this.storage.set('id_token', data.token);
 
-      // Fetch profile information
-      this.lock.getUserInfo(result.accessToken, (error, profile) => {
-
-        if (error) {
-          console.log(error);
-          return;
-        }
-
-        profile.user_metadata = profile.user_metadata || {};
-        this.storage.set('profile', JSON.stringify(profile));
-        this.user = profile;
+              //Set user
+              this.user = data.user;
+              this.storage.set('user', JSON.stringify(data.user));
+            });
       });
 
       this.lock.hide();
-
-      this.storage.set('refresh_token', result.refreshToken);
-      this.zone.run(() => this.user = result.profile);
-
     });
   }
 
@@ -103,10 +87,11 @@ export class AuthService {
    * Removes all authentication parameters from the application.
    */
   public logout() {
-    this.storage.remove('profile');
+    this.authHttp.get(environment.server_url + '/api/logout');
+
+    this.storage.remove('user');
     this.storage.remove('id_token');
     this.idToken = null;
-    this.storage.remove('refresh_token');
     this.zone.run(() => this.user = null);
 
     // Unschedule the token refresh
