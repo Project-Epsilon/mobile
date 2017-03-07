e209b376719fd2c71e23ceea99749da2a826f9bb
Peter0892 <peter_04man@hotmail.com>
2017-03-06 23:35:43 -0500

    Refactored deposit by moving its methods into the deposit component rather than inside of manage

--- src/pages/manage/deposit/deposit.component.ts
+++ src/pages/manage/deposit/deposit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {BankTransferService} from "../../../providers/bank.service";
 
 @Component({
   selector: 'app-deposit',
@@ -7,9 +8,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepositComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public bankSrv: BankTransferService
+  ) { }
 
   ngOnInit() {
+  }
+
+  //******************************************************
+  // AddMoney
+  //******************************************************
+
+  addMoney = {
+    currency: null,
+    amount: 0,
+    decimalPlaces: 0
+  };
+
+  public setDecimalPlaces() {
+    let minorUnit = this.addMoney.currency.minor_unit;
+    if (minorUnit == 0) {
+      this.addMoney.decimalPlaces = 1;
+    } else {
+      this.addMoney.decimalPlaces = 1.0 / Math.pow(10, minorUnit);
+    }
+  }
+
+  public submitAddMoney() {
+    this.bankSrv.deposit(this.addMoney.amount, this.addMoney.currency.code)
+      .subscribe(res => {
+        console.log(res);
+        /**
+         * Relavent information
+         * data.transactions.description
+         * data.transactions.invoice_number
+         * data.create_time
+         * data.id
+         * data.links[i].href
+         *    0 - GET
+         *    1 - REDIRECT
+         *    2 - POST
+         */
+        // let paypalUrl = res.links[1].href;
+        // let browser = new InAppBrowser(paypalUrl, '_blank', 'location=yes');
+      });
   }
 
 }
--- src/pages/manage/manage.ts
+++ src/pages/manage/manage.ts
@@ -5,6 +5,7 @@ import { BankTransferService } from '../../providers/bank.service';
 import { Storage } from "@ionic/storage";
 import { WalletsService } from "../../providers/wallet.service";
 import {WithdrawComponent} from "./withdraw/withdraw.component";
+import {DepositComponent} from "./deposit/deposit.component";
 
 @Component({
   selector: 'page-manage',
@@ -19,6 +20,7 @@ export class ManagePage {
   loader: Loading;
 
   withdraw: WithdrawComponent;
+  deposit: DepositComponent;
 
   constructor(
     public navCtrl: NavController,
@@ -39,56 +41,19 @@ export class ManagePage {
       this.bankSrv,
       this.walletSrv
     );
+    this.deposit = new DepositComponent(
+      this.bankSrv
+    );
   }
 
   ionViewDidLoad() {
     this.storage.get('currencies')
       .then(currencies => {
         this.currencies = currencies;
-        this.addMoney.currency = this.currencies[0];
-        this.setDecimalPlaces();
+        this.deposit.addMoney.currency = this.currencies[0];
+        this.deposit.setDecimalPlaces();
       });
     this.wallets = this.walletSrv.wallets
-  }
-
-
-  //******************************************************
-  // AddMoney
-  //******************************************************
-
-  addMoney = {
-    currency: null,
-    amount: 0,
-    decimalPlaces: 0
-  };
-
-  public setDecimalPlaces() {
-    let minorUnit = this.addMoney.currency.minor_unit;
-    if (minorUnit == 0) {
-      this.addMoney.decimalPlaces = 1;
-    } else {
-      this.addMoney.decimalPlaces = 1.0 / Math.pow(10, minorUnit);
-    }
-  }
-
-  public submitAddMoney() {
-    this.bankSrv.deposit(this.addMoney.amount, this.addMoney.currency.code)
-      .subscribe(res => {
-        console.log(res);
-        /**
-         * Relavent information
-         * data.transactions.description
-         * data.transactions.invoice_number
-         * data.create_time
-         * data.id
-         * data.links[i].href
-         *    0 - GET
-         *    1 - REDIRECT
-         *    2 - POST
-         */
-        // let paypalUrl = res.links[1].href;
-        // let browser = new InAppBrowser(paypalUrl, '_blank', 'location=yes');
-      });
   }
 
 }
