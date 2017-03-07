3255aba6129636998aaa39579cbe612bd2a596e5
ronnpang1 <ronn.pang@gmail.com>
2017-03-06 19:12:00 -0500

    Deleted changes and fixed spaces

--- src/providers/auth.service.spec.ts
+++ src/providers/auth.service.spec.ts
@@ -1,23 +1,19 @@
 import { Storage } from '@ionic/storage';
 import { AuthHttp, tokenNotExpired } from 'angular2-jwt';
 import { Injectable, NgZone } from '@angular/core';
-
 import { environment } from '../environments/environment';
-import {AuthService} from './auth.service';
-
-
-
+import { AuthService } from './auth.service';
+/** 
+ * Auth test suite
+ */
 
 describe('Auth Service', () => {
     beforeEach(() => {
        this.AuthService = new AuthService();
-    
-    });
+      });
   
      it('Auth authenticated response should not be null', () => {
-        
-        
         expect(this.AuthService.authenticated()).not.toBeNull();
-    });
+      });
   
     });
