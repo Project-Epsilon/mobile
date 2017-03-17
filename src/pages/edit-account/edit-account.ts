6604976e1642759619e91dd9b848c106a4aedcab
Peter0892 <peter_04man@hotmail.com>
2017-03-17 13:59:11 -0400

    added basic validation and error messages for edit account

--- src/pages/edit-account/edit-account.ts
+++ src/pages/edit-account/edit-account.ts
@@ -23,10 +23,10 @@ export class EditAccountPage {
     this.user = Object.assign({}, this.auth.user);
 
     this.updateAccount = this.formBuilder.group({
-      name: [this.user.name, Validators.required],
-      email: [this.user.email, Validators.required],
-      phone_number: [this.user.phone_number, Validators.required],
-      username: [this.user.username, Validators.required],
+      name: [this.user.name, Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      email: [this.user.email, Validators.compose([Validators.required])],
+      phone_number: [this.user.phone_number, Validators.compose([Validators.minLength(10), Validators.maxLength(11), Validators.pattern('[0-9]*'), Validators.required])],
+      username: [this.user.username, Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z0-9]*'), Validators.required])],
     });
   }
 
