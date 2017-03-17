d2e589d394e1eb82c0026d0ded8b674855451736
Peter0892 <peter_04man@hotmail.com>
2017-03-17 15:45:23 -0400

    [Epsilon-EP-44] added email validation for edit account

--- src/pages/edit-account/edit-account.ts
+++ src/pages/edit-account/edit-account.ts
@@ -23,27 +23,37 @@ export class EditAccountPage {
     this.user = Object.assign({}, this.auth.user);
     this.updateAccount = this.formBuilder.group({
       name: [this.user.name,
-        Validators.compose([
-          Validators.maxLength(30),
-          Validators.pattern('[a-zA-Z ]*'),
-          Validators.required]
+        Validators.compose(
+          [
+            Validators.maxLength(30),
+            Validators.pattern('[a-zA-Z ]*'),
+            Validators.required
+          ]
         )],
       email: [this.user.email,
-        Validators.compose([
-          Validators.required]
+        Validators.compose(
+          [
+            Validators.maxLength(255),
+            Validators.pattern("[a-zA-Z]{1,}([a-zA-Z0-9\_\.\-]*)*@[a-zA-Z]{1,}[\.]{1}[a-zA-Z]{1,}"),
+            Validators.required
+          ]
         )],
       phone_number: [this.user.phone_number,
-        Validators.compose([
-          Validators.minLength(10),
-          Validators.maxLength(11),
-          Validators.pattern('[0-9]*'),
-          Validators.required]
+        Validators.compose(
+          [
+            Validators.minLength(10),
+            Validators.maxLength(11),
+            Validators.pattern('[0-9]*'),
+            Validators.required
+          ]
         )],
       username: [this.user.username,
-        Validators.compose([
-          Validators.maxLength(30),
-          Validators.pattern('[a-zA-Z]*'),
-          Validators.required]
+        Validators.compose(
+          [
+            Validators.maxLength(30),
+            Validators.pattern('[a-zA-Z]*'),
+            Validators.required
+          ]
         )],
     });
   }
