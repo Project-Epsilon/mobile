26cd42edc5c0d4a8c614977945d02f56f480dd97
JTalarico <james.v.talarico@gmail.com>
2017-03-17 18:25:19 -0400

    Added a loader for the home page.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,8 +2,7 @@ import { Component } from "@angular/core";
 import { Storage } from "@ionic/storage";
 
 import { Http } from "@angular/http";
-import { App, NavController } from "ionic-angular";
-import { environment } from "../../environments/environment";
+import { App, NavController, LoadingController, Loading } from "ionic-angular";
 import { AuthService } from "../../providers/auth.service";
 import {CurrencyService} from "../../providers/currency.service";
 import { WalletsService } from "../../providers/wallet.service";
@@ -15,8 +14,9 @@ import { LoginPage } from "../login/login";
 })
 export class HomePage {
 
-  wallets: Object = [];
-  currentWalletIndex: number = -1;
+  public wallets: Object = [];
+  public currentWalletIndex: number = -1;
+  private loader: Loading;
 
   constructor(
       public navCtrl: NavController,
@@ -26,23 +26,32 @@ export class HomePage {
       public walletSrv: WalletsService,
       public currencySrv: CurrencyService,
       public http: Http,
-  ) {}
+      public loadingCtrl: LoadingController,
+  ) {
+    this.loader = this.loadingCtrl.create({
+      content: "Loggin in.",
+    });
 
-  /**
-   * Handles the logout process
-   */
-  logout(){
-    this.auth.logout();
-    this.app.getRootNav().setRoot(LoginPage);
-  }
-
-  ionViewDidLoad(){
+    this.loader.present().catch(f => f);
     this.currencySrv.init();
 
     this.walletSrv.getWallets()
       .subscribe((wallets) => {
         this.wallets = wallets;
+        this.loader.dismiss().catch(f => f);
       });
+  }
+
+  /**
+   * Handles the logout process
+   */
+  public logout(){
+    this.auth.logout();
+    this.app.getRootNav().setRoot(LoginPage).catch(f => f);
+  }
+
+  public ionViewDidLoad() {
+
   }
 
 }
