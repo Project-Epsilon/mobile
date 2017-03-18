8a452d80728692bf7207f318bf73b792f381e6d6
Alessandro Power <alessandrojpower@gmail.com>
2017-03-18 16:49:35 -0400

    Add spaces within import braces.

--- src/pages/more/more.ts
+++ src/pages/more/more.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 import { NavController, NavParams } from "ionic-angular";
-import {ContactPage} from "../contact/contact";
+import { ContactPage } from "../contact/contact";
 import { EditAccountPage } from "../edit-account/edit-account";
 
 /*
