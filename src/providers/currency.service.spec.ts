69a09778675b1f83b62d5acf97d70320521ca6fc
Laurendy Lam <lamlaurendy@gmail.com>
2017-03-17 21:32:12 -0400

    format tabs and spaces

--- src/providers/currency.service.spec.ts
+++ src/providers/currency.service.spec.ts
@@ -9,36 +9,35 @@ import { CurrencyService } from "./currency.service";
  * Currency test suite
  */
 describe('Currency Service', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: Http, useFactory: (backend, options) => {
+          return new Http(backend, options);
+        },
+          deps: [MockBackend, BaseRequestOptions]
+        },
+        Storage,
+        MockBackend,
+        BaseRequestOptions,
+        CurrencyService
+      ]
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            providers: [
-                {
-                    provide: Http, useFactory: (backend, options) => {
-                    return new Http(backend, options);
-                },
-                    deps: [MockBackend, BaseRequestOptions]
-                },
-                Storage,
-                MockBackend,
-                BaseRequestOptions,
-                CurrencyService
-            ]
-
-        });
     });
+  });
 
-    it('should construct', async(inject([CurrencyService], (service) => {
-        expect(service).toBeDefined();
-    })));
+  it('should construct', async(inject([CurrencyService], (service) => {
+    expect(service).toBeDefined();
+  })));
 
-    it('get currencies should return', async(inject([CurrencyService], (service) => {
-        expect(service.init()).not.toBeNull();
-    })));
+  it('get currencies should return', async(inject([CurrencyService], (service) => {
+    expect(service.init()).not.toBeNull();
+  })));
 
-    it('USD currency should return', async(inject([CurrencyService], (service) => {
-        expect(service.getCurrency('USD')).not.toBeNull();
-    })));
+  it('USD currency should return', async(inject([CurrencyService], (service) => {
+    expect(service.getCurrency('USD')).not.toBeNull();
+  })));
 
 });
   
