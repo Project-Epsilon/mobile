d30e3f353be413a1337efa75d31b1e05d031078a
Alessandro Power <alessandrojpower@gmail.com>
2017-03-31 14:08:46 -0400

    EP-94 #comment Added logout functionality.

--- src/pages/more/more.ts
+++ src/pages/more/more.ts
@@ -1,7 +1,10 @@
 import { AlertController } from 'ionic-angular';
+import { App } from "ionic-angular";
+import { AuthService } from "../../providers/auth.service";
 import { Component } from "@angular/core";
 import { ContactPage } from "../contact/contact";
 import { EditAccountPage } from "../edit-account/edit-account";
+import { LoginPage } from "../login/login";
 
 
 @Component({
@@ -13,17 +16,31 @@ export class MorePage {
   public contactsPage: any;
   public editAccountPage: any;
 
-  constructor(public alertCtrl: AlertController) {
+  constructor(
+      public alertCtrl: AlertController,
+      public app: App,
+      public auth: AuthService,
+  ) {
     this.contactsPage = ContactPage;
     this.editAccountPage = EditAccountPage;
   }
 
-  logoutPrompt() {
+  public logoutPrompt() {
     let alert = this.alertCtrl.create({
       title: 'Sign Out',
       subTitle: 'Are you sure you want to sign out of mBarter?',
-      buttons: ['Yes', 'No']
+      buttons: [{
+        text: 'Yes',
+        handler: () => {
+          this.logout();
+        }
+      }, 'No']
     });
     alert.present();
+  }
+
+  public logout() {
+    this.auth.logout();
+    this.app.getRootNav().setRoot(LoginPage).catch((f) => f);
   }
 }
