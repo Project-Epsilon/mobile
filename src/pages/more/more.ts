d12b035af9e4e7baa2be82d25366da8601594386
Alessandro Power <alessandrojpower@gmail.com>
2017-03-31 13:50:19 -0400

    EP-94 #comment Added sign out alert.

--- src/pages/more/more.ts
+++ src/pages/more/more.ts
@@ -1,6 +1,8 @@
+import { AlertController } from 'ionic-angular';
 import { Component } from "@angular/core";
 import { ContactPage } from "../contact/contact";
 import { EditAccountPage } from "../edit-account/edit-account";
+
 
 @Component({
   selector: "page-more",
@@ -11,8 +13,17 @@ export class MorePage {
   public contactsPage: any;
   public editAccountPage: any;
 
-  constructor() {
+  constructor(public alertCtrl: AlertController) {
     this.contactsPage = ContactPage;
     this.editAccountPage = EditAccountPage;
+  }
+
+  logoutPrompt() {
+    let alert = this.alertCtrl.create({
+      title: 'Sign Out',
+      subTitle: 'Are you sure you want to sign out of mBarter?',
+      buttons: ['Yes', 'No']
+    });
+    alert.present();
   }
 }
