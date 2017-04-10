b4b00aed29ef30262d81cea1eae6275fa950043e
James Talarico <james.v.talarico@gmail.com>
2017-04-09 21:01:31 -0400

    Transfer fix (#97)
    * Added transfer modal to front page and changed empty wallet message to be less offensive.
    
    * EP-139 #comment Added no match to deep link to fix uncaught promise error.
    
    * Refactored pending transactions to be called incoming. This matches the UI and is also more intuitive.
    
    * Better error handling inside of addTransfer transfer.
    
    * Added received at tag in modal.

--- src/pages/transfers/transfers.ts
+++ src/pages/transfers/transfers.ts
@@ -62,11 +62,7 @@ export class TransfersPage {
     this.transfSrv.getTransferByToken(transferToken)
       .subscribe(
         (res) => {
-          this.loader.dismiss().catch((f) => f);
-          let transfer = <any> res;
-          let transferWithToken = transfer.data;
-          transferWithToken.token = transferToken;
-          this.incomingTransfers.push(transferWithToken);
+          this.handleAddTransaction(res, transferToken);
         },
         (err) => {
           this.loader.dismiss().catch((f) => f);
@@ -91,6 +87,25 @@ export class TransfersPage {
         }
       },
     );
+  }
+
+  /**
+   * On success adds the transfer to the users list. On fail, alerts the user.
+   *
+   * @param res
+   * @param transferToken
+   */
+  private handleAddTransaction (res, transferToken) {
+    this.loader.dismiss().catch((f) => f);
+
+    if (res.data) {
+      let transfer = <any> res;
+      let transferWithToken = transfer.data;
+      transferWithToken.token = transferToken;
+      this.incomingTransfers.push(transferWithToken);
+    } else {
+      Alert(this.alertCtrl, "Whoops!", res._body.json().errors.message, ["Dismiss."]);
+    }
   }
 
 }
