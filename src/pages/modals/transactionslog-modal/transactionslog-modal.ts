6d93c1efe6efdc6d871d780b733ee051d45b8fae
Peter0892 <peter_04man@hotmail.com>
2017-04-10 11:25:38 -0400

    corrected "To" and "From" keywords to match outgoing and incoming transfers modals (#100)

--- src/pages/modals/transactionslog-modal/transactionslog-modal.ts
+++ src/pages/modals/transactionslog-modal/transactionslog-modal.ts
@@ -8,11 +8,22 @@ import { ViewController, NavParams } from "ionic-angular";
 export class TransactionsLogModalPage {
 
   public transfer;
+  public to_or_from;
+  public send_or_receive;
 
   constructor(
     public viewCtrl: ViewController,
     public params: NavParams) {
     this.transfer = this.params.get("transfer");
+
+    if(this.transfer.amount > 0) {
+      this.to_or_from = "From";
+      this.send_or_receive = "Received on: ";
+    }
+    else if(this.transfer.amount < 0){
+      this.to_or_from = "To";
+      this.send_or_receive = "Sent on: ";
+    }
   }
 
   public dismiss() {
