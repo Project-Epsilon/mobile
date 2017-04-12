94d9372afd5915c69d9f6edbb6ead5e4a305f38a
Laurendy Lam <lamlaurendy@gmail.com>
2017-04-11 22:34:48 -0400

    update object reference

--- src/components/transaction-log/transaction-log.component.ts
+++ src/components/transaction-log/transaction-log.component.ts
@@ -1,26 +1,11 @@
-import { OnChanges, Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
   selector: "transaction-log",
   templateUrl: "transaction-log.component.html",
 })
-export class TransactionLogComponent implements OnInit, OnChanges {
+export class TransactionLogComponent {
 
   @Input() public wallet: any;
-  public transactions: any;
 
-  /**
-   * On input changes. Update the transactions.
-   */
-  public ngOnChanges() {
-    this.transactions = this.wallet.transactions.data.reverse();
-  }
-
-  /**
-   * Does logic.
-   */
-  public ngOnInit () {
-    console.log('Hello1');
-    this.transactions = this.wallet.transactions.data.reverse();
-  }
 }
